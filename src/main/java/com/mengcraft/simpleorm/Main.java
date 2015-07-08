6da9322af85eed52e759cdf6f3ca4b92dbe51b17
caoli5288 <caoli5288@gmail.com>
2015-07-09 02:28:29 +0800

    Put manager object into bukkit's build-in service manager.

--- src/main/java/com/mengcraft/simpleorm/Main.java
+++ src/main/java/com/mengcraft/simpleorm/Main.java
@@ -1,8 +1,17 @@
 package com.mengcraft.simpleorm;
 
+import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+    
+    @Override
+    public void onLoad() {
+        getServer().getServicesManager().register(EbeanManager.class,
+                EbeanManager.DEFAULT,
+                this,
+                ServicePriority.Normal);
+    }
     
     @Override
     public void onEnable() {
