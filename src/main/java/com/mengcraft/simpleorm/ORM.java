e733ec2300a81805d5b759a8d5f1c6f97210eb22
caoli5288 <caoli5288@gmail.com>
2017-12-15 18:29:07 +0800

    Add manage command

--- src/main/java/com/mengcraft/simpleorm/ORM.java
+++ src/main/java/com/mengcraft/simpleorm/ORM.java
@@ -4,10 +4,14 @@ import com.mengcraft.simpleorm.lib.LibraryLoader;
 import com.mengcraft.simpleorm.lib.MavenLibrary;
 import lombok.SneakyThrows;
 import lombok.val;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Arrays;
+import java.util.Iterator;
 
 public class ORM extends JavaPlugin {
 
@@ -37,14 +41,57 @@ public class ORM extends JavaPlugin {
         new MetricsLite(this).start();
     }
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        val manager = EbeanManager.DEFAULT;
-        val l = manager.map;
-        if (!l.isEmpty()) {
-            l.forEach((key, handler) -> sender.sendMessage("[" + key + "] -> " + handler));
+    public boolean onCommand(CommandSender who, Command command, String label, String[] input) {
+        if (input.length < 1) {
+            for (val executor : SubExecutor.values()) {
+                who.sendMessage('/' + label + executor.usage);
+            }
+        } else {
+            try {
+                Iterator<String> itr = Arrays.asList(input).iterator();
+                SubExecutor.valueOf(itr.next().toUpperCase()).exec(who, itr);
+                return true;
+            } catch (Exception exc) {
+                who.sendMessage(ChatColor.RED + exc.toString());
+            }
         }
-        return true;
+        return false;
+    }
+
+    enum SubExecutor {
+
+        LIST("list") {
+            void exec(CommandSender who, Iterator<String> itr) {
+                val all = EbeanManager.DEFAULT.map;
+                if (!all.isEmpty()) {
+                    all.forEach((key, handler) -> who.sendMessage("[" + key + "] -> " + handler));
+                }
+            }
+        },
+
+        REMOVE("remove <plugin_name>") {
+            void exec(CommandSender who, Iterator<String> itr) {
+                EbeanHandler remove = EbeanManager.DEFAULT.map.remove(itr.next());
+                who.sendMessage(remove == null ? "handle not found" : "okay");
+            }
+        },
+
+        REMOVE_ALL("remove_all") {
+            void exec(CommandSender who, Iterator<String> itr) {
+                EbeanManager.DEFAULT.map.clear();
+                who.sendMessage("okay");
+            }
+        };
+
+        private final String usage;
+
+        SubExecutor(String usage) {
+            this.usage = usage;
+        }
+
+        void exec(CommandSender who, Iterator<String> itr) {
+            throw new AbstractMethodError("exec");
+        }
     }
 
 }
