da9ca5ef9187c408e333ef91ced1894b150301cd
caoli5288 <caoli5288@gmail.com>
2019-08-29 17:02:31 +0800

    GenericTrigger.java

--- src/main/java/com/mengcraft/simpleorm/GenericTrigger.java
+++ src/main/java/com/mengcraft/simpleorm/GenericTrigger.java
@@ -1,36 +1,41 @@
 package com.mengcraft.simpleorm;
 
 import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Multimap;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.json.simple.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 import java.util.function.BiConsumer;
 
+/**
+ * @deprecated Still unstable
+ */
 public class GenericTrigger {
 
     private final Multimap<String, TriggerListener> functions = ArrayListMultimap.create();
 
-    public TriggerListener on(@NonNull String category, @NonNull BiConsumer<JSONObject, JSONObject> function) {
+    public TriggerListener on(@NonNull String category, @NonNull BiConsumer<ImmutableMap<String, Object>, Map<String, Object>> function) {
         TriggerListener listener = new TriggerListener(category, function);
         functions.put(category, listener);
         return listener;
     }
 
-    public JSONObject trigger(@NonNull String category, @NonNull JSONObject params) {
-        JSONObject object = new JSONObject();
+    public Map<String, Object> trigger(@NonNull String category, @NonNull ImmutableMap<String, Object> params) {
+        Map<String, Object> object = new HashMap<>();
         for (TriggerListener listener : functions.get(category)) {
             listener.function.accept(params, object);
         }
         return object;
     }
 
-    public JSONObject trigger(@NonNull String category) {
-        return trigger(category, new JSONObject());
+    public Map<String, Object> trigger(@NonNull String category) {
+        return trigger(category, ImmutableMap.of());
     }
 
     @EqualsAndHashCode(exclude = "function")
@@ -39,7 +44,7 @@ public class GenericTrigger {
 
         private final UUID id = UUID.randomUUID();
         private final String category;
-        private final BiConsumer<JSONObject, JSONObject> function;
+        private final BiConsumer<ImmutableMap<String, Object>, Map<String, Object>> function;
 
         public void cancel() {
             functions.remove(category, this);
