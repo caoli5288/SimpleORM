09e105609dbf39a74757da7f05d11809c28078ca
caoli5288 <caoli5288@gmail.com>
2020-04-02 11:27:40 +0800

    RedisLiveObjectBucket.java

--- src/main/java/com/mengcraft/simpleorm/redis/RedisLiveObjectBucket.java
+++ src/main/java/com/mengcraft/simpleorm/redis/RedisLiveObjectBucket.java
@@ -11,12 +11,16 @@ public class RedisLiveObjectBucket {
     private final RedisWrapper redisWrapper;
     private final String id;
 
-    public RedisLiveObject get(String id) {
-        return new RedisLiveObject(this, id);
+    public RedisLiveObject get(String obj) {
+        return new RedisLiveObject(this, obj);
     }
 
     public void remove(String object) {
         redisWrapper.open(jedis -> jedis.del(id + ":" + object));
+    }
+
+    public boolean contains(String obj) {
+        return redisWrapper.call(jedis -> jedis.exists(id + ":" + obj));
     }
 
     /**
